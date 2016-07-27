8c9128161af682f61995f3ab4d35455593484604
JeffCZ <179357696@qq.com>
2016-07-27 20:06:56 +0800

    page

---
+++ README.md
@@ -0,0 +1,43 @@
+﻿#frame_player#
+
+一个微小的帧动画组件，参考demo
+
+相关参数：
+
+	{
+		// canvas的id
+        id: "player",
+		// 动画总时长。这里考虑到帧动画图片一般由一段视频导出，可以方便地拿到总时长，所以使用总时长来控制动画速度
+        duration: 800,
+		// 图片名数组
+        imgs: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png'],
+		// 图片路径
+        imgsUrl: './images/',
+    // 是否使用雪碧图，目前雪碧图默认为横向排列，待优化
+        useSpriteImg: false
+		// 加载完自动播放
+        autoplay: false,
+		// 循环播放
+        loop: false,
+		// 显示第一帧
+        showFirstFrame: true,
+		// 加载完成cb
+        onReady: function() {
+            console.log(this.framesCount)
+        },
+		// 开始播放cb
+        onPlay: function() {
+            console.log('play')
+        },
+		// 暂停cb
+        onPause: function() {
+            console.log('pause')
+        },
+		// 播放结束cb
+        onEnd: function() {
+            console.log('end')
+        }
+    }
+
+
+
---
+++ frameplayer.js
@@ -0,0 +1,118 @@
+var FramePlayer = (function(){
+	function FP(paras){
+    this.cav = document.getElementById(paras.id);
+    this.ctx = this.cav.getContext('2d');
+    this.width = this.cav.width;
+    this.height = this.cav.height;
+    this.duration = paras.duration || 1000;
+    this.imgUrl = paras.imgUrl || 'images/';
+    this.imgs = paras.imgs || [];
+    this.imgsCount = this.imgs.length;
+    this.useSpriteImg = paras.useSpriteImg || (this.imgsCount == 1), //目前假设雪碧图为横向排列，待优化
+    this.autoplay = paras.autoplay || false;
+    this.showFirstFrame = paras.showFirstFrame || false;
+    this.loop = paras.loop || false;
+    this.frames = [];
+    this.framesCount = 0;
+    this.currFrameIndex = 0;
+    this.readyTag = false;
+    this.playingTag = false;
+    this.playInterval = null;
+    this.onReady = paras.onReady;
+    this.onPlay = paras.onPlay;
+    this.onPause = paras.onPause;
+    this.onEnd = paras.onEnd;
+    this.onInit = paras.onInit;
+    this.cache = document.createElement('canvas');
+    this.cache.width = this.width;
+    this.cache.height = this.height;
+    this.cacheCtx = this.cache.getContext('2d');
+    this.init();
+	}
+  FP.prototype.loadImg = function(src, index){
+    var _this = this,
+        img = new Image();
+    img.onload = function(){
+      _this.frames[index] = this;
+      _this.framesCount ++;
+      if(_this.framesCount >= _this.imgsCount){
+        if(_this.useSpriteImg){
+          _this.framesCount = _this.frames[0].width / _this.width;
+        }
+        _this.ready();
+      }
+    }
+    img.src = src;
+  }
+  FP.prototype.preLoadImg = function(){
+    for(var i = 0; i < this.imgsCount; i++){
+      this.loadImg(this.imgUrl + this.imgs[i], i);
+    }
+  }
+  FP.prototype.ready = function(){
+    this.readyTag = true;
+    if(this.onReady) this.onReady();
+    //加载完成之后做canvas缓存 待优化
+    if(this.useSpriteImg){
+      this.cacheCtx.drawImage(this.frames[0], 0, 0);
+    }else{
+      for(var i = 0; i < this.framesCount; i++){
+        this.cacheCtx.drawImage(this.frames[i], 0, 0);
+      }
+    }
+    
+    if(this.showFirstFrame) this.draw();
+    if(this.autoplay) this.play();
+  }
+  FP.prototype.draw = function(){
+    var _this = this,
+        currFrame = _this.frames[_this.currFrameIndex];
+    _this.ctx.clearRect(0, 0, _this.width, _this.height);
+    if(_this.useSpriteImg){
+      _this.ctx.drawImage(_this.frames[0], (0-_this.currFrameIndex)*_this.width, 0);
+    }else{
+      _this.ctx.drawImage(currFrame, 0, 0);
+    }
+  }
+  FP.prototype.play = function(){
+    var _this = this;
+    if(!_this.playingTag){
+      if(_this.onPlay) _this.onPlay();
+      _this.playingTag = true;
+      
+      
+      _this.draw();
+      _this.playInterval = setInterval(function(){
+        _this.currFrameIndex ++;
+        
+        if(_this.currFrameIndex >= _this.framesCount){
+          clearInterval(_this.playInterval);
+          _this.end();
+        }else{
+          _this.draw();
+        }
+      }, _this.duration / _this.framesCount);
+      
+      
+    }
+  }
+  FP.prototype.pause = function(){
+    var _this = this;
+    clearInterval(_this.playInterval);
+    _this.playingTag = false;
+    if(_this.onPause) _this.onPause();
+  }
+  FP.prototype.end = function(){
+    var _this = this;
+    _this.playingTag = false;
+    _this.currFrameIndex = 0;
+    if(_this.onEnd) _this.onEnd();
+    if(_this.loop) _this.play();
+  }
+  FP.prototype.init=function(){
+    var _this = this;
+    if(_this.onInit) _this.onInit();
+    _this.preLoadImg();
+  }
+	return FP;
+})();
